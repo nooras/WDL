6fae7dca15163f0c1a39b5db10eaf2c3fe3c9160
nooras <anoorasfatima@gmail.com>
2018-08-07 12:31:19 +0530

    marksheet and loadxml

---
+++ Github_work/16co01/js/app.js
@@ -0,0 +1,6 @@
+var elements = document.getElementsByTagName('h1');
+var ele = document.getElementsByClassName('d');
+console.log(elements[0].textContent);
+console.log(elements.length);
+console.log(ele);
+console.log(ele.innerHTML);
